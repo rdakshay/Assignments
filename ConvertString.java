a65ef4c1b704f50f546b1eeefd2d4286bcbf62b2
rdakshay <120654036+rdakshay@users.noreply.github.com>
2023-01-04 00:08:52 +0530

    Update ConvertString.java

--- ConvertString.java
+++ ConvertString.java
@@ -4,12 +4,11 @@ public class ConvertString
 {
 public static void main(String[] args)
 {
-String word="rolex from vikram";
+String word="b.v.raju college";
 System.out.println("Statement Before Converting: \n\n"+word);
-// We Need To Convert The Word rolex To ROLEX and vikram To VIKRAM
+// We Need To Convert The Word raju To RAJU
 System.out.println("\nStatement After Converting is: ");
 System.out.println("-------------------------------");
-System.out.println(word.replace("rolex","ROLEX"));
-System.out.println(word.replace("vikram","VIKRAM"));  
+System.out.println(word.replace("raju","RAJU")); 
 }
 }
