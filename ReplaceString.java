9bb7e4c80adb2897f7501184f9c61426c78badf2
rdakshay <120654036+rdakshay@users.noreply.github.com>
2023-01-14 15:08:48 +0530

    Add files via upload

---
+++ ReplaceString.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		       // Consider The Example in Question
+                          String str="bvrit college";
+                          System.out.println("Statement before Change : \n"+str);
+                          System.out.println("-------------------------------");
+                          System.out.println("Statement after Change : \n"+str.replace("ll","lL"));
+	}
+
+}
